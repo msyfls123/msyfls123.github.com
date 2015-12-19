b6c46b9bf918312cd9901203565692b9d8a3eb2f
msyfls123 <m18771058712@live.com>
2015-12-19 11:05:50 +0800

    ChangeStyle

--- js/graphic.js
+++ js/graphic.js
@@ -2,7 +2,7 @@ $(function() {
 	 $("#cas").html5_3d_animation({
 	        window_width: '1000',
 	        window_height: '400',
-	        window_background: '#112238',
+	        window_background: '#070707',
 	        star_count: '1000',
 	        star_color: '#bcd',
 	        star_depth: '200'
